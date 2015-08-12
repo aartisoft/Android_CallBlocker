e62f5f6bdfac1e74a80099bd0545ed10dcc48424
Md. Nazmul Hasan Masum <mnhmasum@gmail.com>
2015-08-12 20:23:28 +0600

    asfsaas

---
+++ app/src/main/java/com/codersact/smsblock/blockedsms/BlockedListView.java
@@ -0,0 +1,10 @@
+package com.codersact.smsblock.blockedsms;
+
+/**
+ * Created by masum on 11/08/2015.
+ */
+public interface BlockedListView {
+    void getSmsInfo();
+    String getSmsName();
+    String getSmsNumber();
+}
